1c07665afc5784ab9a4d869cb085837725050b5e
Graeme Rockhill <graemerockhill@gmail.com>
2021-09-24 18:53:21 +0100

    added hediff stungun

---
+++ Source/RoyalLegion/ClassLibrary1/ModExtension_HediffBullet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+
+namespace RoyalLegion
+{
+    public class ModExtension_HediffBullet : DefModExtension
+    {
+        public float addHediffChance = 0.25f;
+        public HediffDef hediffToAdd;
+    }
+}
---
+++ Source/RoyalLegion/ClassLibrary1/Projectile_HediffBullet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Verse;
+using RimWorld;
+
+namespace RoyalLegion
+{
+    public class Projectile_HediffBullet : Bullet
+    {
+        public ModExtension_HediffBullet Props => base.def.GetModExtension<ModExtension_HediffBullet>();
+
+        protected override void Impact(Thing hitThing)
+        {
+            base.Impact(hitThing);
+            if (Props != null && hitThing != null && hitThing is Pawn hitPawn)
+            {
+                float rand = Rand.Value;
+                if (rand <= Props.addHediffChance)
+                {
+                    Messages.Message("HediffBullet_SuccessMessage".Translate(
+                        this.launcher.Label, hitPawn.Label
+                    ), MessageTypeDefOf.NeutralEvent);
+                    Hediff plagueOnPawn = hitPawn.health?.hediffSet?.GetFirstHediffOfDef(Props.hediffToAdd);
+                    float randomSeverity = Rand.Range(0.15f, 0.30f);
+                    if (plagueOnPawn != null)
+                    {
+                        plagueOnPawn.Severity += randomSeverity;
+                    }
+                    else
+                    {
+                        Hediff hediff = HediffMaker.MakeHediff(Props.hediffToAdd, hitPawn);
+                        hediff.Severity = randomSeverity;
+                        hitPawn.health.AddHediff(hediff);
+                    }
+                }
+                else
+                {
+                    MoteMaker.ThrowText(hitThing.PositionHeld.ToVector3(), hitThing.MapHeld, "HediffBullet_FailureMote".Translate(Props.addHediffChance), 12f);
+                }
+            }
+        }
+    }
+}
